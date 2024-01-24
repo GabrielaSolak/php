9cd043b4f7fc62c464981b39a1d802a62617dcab
grybrys <113046543+GabrielaSolak@users.noreply.github.com>
2024-01-24 08:36:44 +0100

    skrypt.php

--- formularz/skrypt.php
+++ formularz/skrypt.php
@@ -5,21 +5,34 @@ Funkcje:
 - empty() - sprawdza czy w tablicy pod podanym kluczem nie ma wartosci
 
 metoda - tablica
-get - $_GET             $_GET['klucz']
-post - $_POST           $_POST['klucz'] wartosci atrybutu name kontrolek formularza  
+get - $_GET             $_GET['klucz'] (jawne dane)
+post - $_POST           $_POST['klucz'] wartosci atrybutu name kontrolek formularza 
 */
 
-echo "<u>Przesłałeś następujące informacje:</u> <br>";
-echo "Imie -> ".$_GET['imie']."<br>";
-echo "Nazwisko -> ".$_GET['nazwisko']."<br>";
-echo "Wiek -> ".$_GET['wiek']."<br>";
-echo "Email -> ".$_GET['email']."<br>";
-echo "Wyksztalczenie -> ".$_GET['w']."<br>";
-echo "Zgoda -> ".$_GET['zgoda']."<br>";
+echo "<u>Przesłałeś następujące informacje:</u> <br><br>";
+echo "Imie -> ".$_POST['imie']."<br>";
+echo "Nazwisko -> ".$_POST['nazwisko']."<br>";
+echo "Wiek -> ".$_POST['wiek']."<br>";
+echo "Email -> ".$_POST['email']."<br>";
+echo "Wyksztalczenie -> ".$_POST['w']."<br>";
+//echo "Język -> ".$_POST['jezyk']."<br>";
+//echo "Język -> ".$_POST['jezyk'][0]."<br>"; //tworzy sie tablica dwuwymiarowa;
+//sposob 1
+echo "Język -> ";
+foreach($_POST['jezyk'] as $j)
+{
+    echo $j.", ";
+}
+echo "<br>";
+//sposob 2
+echo 'Jezyk: ';
+echo "<ul>";
+foreach($_POST['jezyk'] as $j)
+{
+    echo "<li>".$j."</li>";
+}
+echo "</ul><br>";
 
-
-
-
-
+echo "Zgoda -> ".$_POST['zgoda']."<br>";
 
 ?>
