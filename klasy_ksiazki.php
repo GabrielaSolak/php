9fd230012e5f7e5567106feda8cdd350ede369b3
grybrys <113046543+GabrielaSolak@users.noreply.github.com>
2024-01-28 18:12:41 +0100

    klasy_ksiazki.php

--- klasy_ksiazki.php
+++ klasy_ksiazki.php
@@ -2,7 +2,8 @@
 
 class Ksiazka
 {
-    public $tytul, $autor, $rok_wydania, $wydawnictwo, $gatunek, $identyfikator;
+    public $tytul, $rok_wydania, $wydawnictwo;
+    private $autor, $gatunek, $identyfikator;
 
     function wpiszTytul($arg0){
         $this -> tytul = $arg0;
@@ -74,21 +75,84 @@ $ksiazka2 -> wpiszIdentyfikator(2);
 $ksiazka2 -> wpiszTytul('Komputer świat');
 
 $ksiazka3 = new Ksiazka();
+$ksiazka3 -> wpiszAutora('Remigiusz Mróż');
+$ksiazka3 -> wpiszRokWydania(2020);
+$ksiazka3 -> wpiszWydawnictwo('lubimyCzytać');
+$ksiazka3 -> wpiszGatunek('powieść');
+$ksiazka3 -> wpiszIdentyfikator(3);
+$ksiazka3 -> wpiszTytul('Chyłka - Kasacja');
 
 
+class Czasopismo extends Ksiazka
+{
+    public $nr;
+
+    function wpiszNr($arg6){
+        $this -> nr = $arg6;
+    }
+
+    function pobierzNr(){
+        return $this -> nr; 
+    }
+}
+
+$czasopismo1 = new Czasopismo;
+$czasopismo1 -> wpiszRokWydania(1998);
+$czasopismo1 -> wpiszWydawnictwo('Ringier Axel Springer Polska');
+$czasopismo1 -> wpiszTytul('Komputer świat - bazy danych');
+$czasopismo1 -> wpiszNr('3/2017');
 
 
+class Multibook extends Czasopismo
+{
+    // if(empty($autor))
+    // {
+    //     $this -> autor = "Nie podano Autora multibooka";
+    // }
+}
 
+$multibook1 = new Multibook;
+$multibook1 -> wpiszAutora('Gabriela Solak');
+$multibook1 -> wpiszRokWydania(2026);
+$multibook1 -> wpiszWydawnictwo('Biuro Literackie');
+$multibook1 -> wpiszGatunek('poezja');
+$multibook1 -> wpiszIdentyfikator(4);
+$multibook1 -> wpiszTytul('Spero in poetica');
+$multibook1 -> wpiszNr('1/2026');
 
-
-
-
-
-
-
-
-
-
-
+echo "Książka1:<br>";
+echo $ksiazka1 -> pobierzAutora()."<br>";
+echo $ksiazka1 -> pobierzTytul()."<br>";
+echo $ksiazka1 -> pobierzRokWydania()."<br>";
+echo $ksiazka1 -> pobierzWydawnictwo()."<br>";
+echo $ksiazka1 -> pobierzGatunek()."<br>";
+echo $ksiazka1 -> pobierzIdentyfikator()."<br><br>";
+echo "Książka2:<br>";
+echo $ksiazka2 -> pobierzAutora()."<br>";
+echo $ksiazka2 -> pobierzTytul()."<br>";
+echo $ksiazka2 -> pobierzRokWydania()."<br>";
+echo $ksiazka2 -> pobierzWydawnictwo()."<br>";
+echo $ksiazka2 -> pobierzGatunek()."<br>";
+echo $ksiazka2 -> pobierzIdentyfikator()."<br><br>";
+echo "Książka3:<br>";
+echo $ksiazka3 -> pobierzAutora()."<br>";
+echo $ksiazka3 -> pobierzTytul()."<br>";
+echo $ksiazka3 -> pobierzRokWydania()."<br>";
+echo $ksiazka3 -> pobierzWydawnictwo()."<br>";
+echo $ksiazka3 -> pobierzGatunek()."<br>";
+echo $ksiazka3 -> pobierzIdentyfikator()."<br><br>";
+echo "Czasopismo1<br>";
+echo $czasopismo1 -> pobierzRokWydania()."<br>";
+echo $czasopismo1 -> pobierzWydawnictwo()."<br>";
+echo $czasopismo1 -> pobierzTytul()."<br>";
+echo $czasopismo1 -> pobierzNr()."<br><br>";
+echo "Multibook1<br>";
+echo $multibook1 -> pobierzAutora()."<br>";
+echo $multibook1 -> pobierzTytul()."<br>";
+echo $multibook1 -> pobierzRokWydania()."<br>";
+echo $multibook1 -> pobierzWydawnictwo()."<br>";
+echo $multibook1 -> pobierzGatunek()."<br>";
+echo $multibook1 -> pobierzIdentyfikator()."<br>";
+echo $multibook1 -> pobierzNr()."<br>";
 
 ?>
