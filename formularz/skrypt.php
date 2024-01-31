709f71ccafdf92aa1daea9357e57958a5385be2e
grybrys <113046543+GabrielaSolak@users.noreply.github.com>
2024-01-31 08:44:41 +0100

    skrypt.php

--- formularz/skrypt.php
+++ formularz/skrypt.php
@@ -5,34 +5,79 @@ Funkcje:
 - empty() - sprawdza czy w tablicy pod podanym kluczem nie ma wartosci
 
 metoda - tablica
-get - $_GET             $_GET['klucz'] (jawne dane)
+get - $_GET             $_GET['klucz'] (jawne dane, dokleja dane do url)
 post - $_POST           $_POST['klucz'] wartosci atrybutu name kontrolek formularza 
 */
 
 echo "<u>Przesłałeś następujące informacje:</u> <br><br>";
-echo "Imie -> ".$_POST['imie']."<br>";
-echo "Nazwisko -> ".$_POST['nazwisko']."<br>";
-echo "Wiek -> ".$_POST['wiek']."<br>";
-echo "Email -> ".$_POST['email']."<br>";
-echo "Wyksztalczenie -> ".$_POST['w']."<br>";
+
+if($_POST['imie'] === ''|| $_POST['nazwisko'] === '' || $_POST['email'] === '') //empty($_POST['email'])
+{
+    echo "<p style='color:red'>Nie wprowadziles podstawowych informacji</p>";
+}
+else{
+    echo "Imie -> ".$_POST['imie']."<br>";
+    echo "Nazwisko -> ".$_POST['nazwisko']."<br>";
+    echo "Email -> ".$_POST['email']."<br>";
+}
+
+if($_POST['wiek'] > 0){
+    echo "Wiek -> ".$_POST['wiek']."<br>";
+}
+else{
+    echo "<p style='color:red'>Nie poprawna wartość w polu wiek</p>";
+}
+
+if(isset($_POST['w'])){ //sprawdza czy jest wartosc, jesli nie to pojawia sie komunikat
+    echo "Wyksztalczenie -> ".$_POST['w']."<br>";
+}
+else{
+    echo "<p style='color:red'>Nie zaznaczyles wykształcenia!</p>";
+}
 //echo "Język -> ".$_POST['jezyk']."<br>";
 //echo "Język -> ".$_POST['jezyk'][0]."<br>"; //tworzy sie tablica dwuwymiarowa;
 //sposob 1
-echo "Język -> ";
-foreach($_POST['jezyk'] as $j)
-{
-    echo $j.", ";
+if(empty($_POST['jezyk'])){ //sprawdza czy pole jest puste
+    echo "<p style='color:red'>Nie zaznaczyles języka! (1)</p>";
 }
-echo "<br>";
+else{
+    echo "Język -> ";
+    foreach($_POST['jezyk'] as $j)
+    {
+        echo $j.", ";
+    }
+    echo "<br>";
+}
 //sposob 2
-echo 'Jezyk: ';
-echo "<ul>";
-foreach($_POST['jezyk'] as $j)
-{
-    echo "<li>".$j."</li>";
+if(empty($_POST['jezyk'])){ //sprawdza czy pole jest puste
+    echo "<p style='color:red'>Nie zaznaczyles języka! (2)</p>";
 }
-echo "</ul><br>";
+else{
+    echo 'Jezyk: ';
+    echo "<ul>";
+    foreach($_POST['jezyk'] as $j)
+    {
+        echo "<li>".$j."</li>";
+    }
+    echo "</ul><br>";
+}
 
-echo "Zgoda -> ".$_POST['zgoda']."<br>";
+if(!isset($_POST['zgoda'])){
+    echo "<p style='color:red'>Nie zostales zarejestrowany</p>";
+}
+else{
+    echo "Zgoda -> ".$_POST['zgoda']."<br><br>";
+}
+
+// scieżka dostępu (tymczasowa lokalizacja) - nazwa pliku - rozszerzenie/typ - rozmiar
+// $_FILES[nazwa_pola][name(nazwa_pliku) || sizee(rozmiarpliku) || type(typ pliku) || tmp_name(lokalizacja tymczasowa)]
+echo "Przekazałeś plik: <br>";
+echo "Nazwa pliku: ".$_FILES['plik1']['name']."<br>";
+echo "Lokalizacja tymczasowa: ".$_FILES['plik1']['tmp_name']."<br>";
+echo "Rozmiar pliku: ".$_FILES['plik1']['size']."<br>";
+echo "Typ pliku: ".$_FILES['plik1']['type']."<br>";
+
+// move_aploaded_file() - przenosi pliki z lokalizacji tymczasowej do lokalizacji docelowej
+// is_uploaded_file - sprawdzqa czy plik został przesłany
 
 ?>
